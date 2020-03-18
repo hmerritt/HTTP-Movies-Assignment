08859b4caefaab4fce38e0d2c3afc55f3a1bc461
Harry Merritt <harry@merritt.es>
2020-03-18 02:18:40 +0000

    Update all cast individually

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -18,7 +18,6 @@ function UpdateMovie({ movieList, setMovieList }) {
             .get(`http://localhost:5000/api/movies/${id}`)
             .then(res => {
                 setMovie(res.data);
-                console.log(res.data);
             })
             .catch(err => console.log(err.response));
     };
@@ -29,12 +28,23 @@ function UpdateMovie({ movieList, setMovieList }) {
 
 
     //  Handle inputs
-    const handleChange = (e) => {
-        setMovie({ ...movie, [e.target.name]: e.target.value });
+    const handleChange = e => {
+        if (e.target.name !== "actor") {
+            setMovie({ ...movie, [e.target.name]: e.target.value });
+        }
+        else {
+            const newStars = [...movie.stars];
+            newStars[Number(e.target.id)] = e.target.value;
+
+            setMovie({
+                ...movie,
+                stars: newStars
+            });
+        }
     };
 
     //  Update movie
-    const updateMovie = (e) => {
+    const updateMovie = e => {
         e.preventDefault();
 
         //  Movie id
@@ -60,16 +70,47 @@ function UpdateMovie({ movieList, setMovieList }) {
 
     return (
         <div className="update-movie">
-            <h3>Update Movie</h3>
+            <h3>Update {movie.title === "" ? "Movie" : movie.title}</h3>
 
-            {
-                movie.title === "" ? "Loading movie information..." : ""
-            }
+            {movie.title === "" ? "Loading movie information..." : ""}
 
             <form onSubmit={updateMovie}>
-                <input type="text" name="title" placeholder="Title" value={movie.title} onChange={handleChange}  />
-                <input type="text" name="director" placeholder="Director" value={movie.director} onChange={handleChange}  />
-                <input type="number" name="metascore" placeholder="Metascore" value={movie.metascore} onChange={handleChange}  />
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Title"
+                    value={movie.title}
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    name="director"
+                    placeholder="Director"
+                    value={movie.director}
+                    onChange={handleChange}
+                />
+                <input
+                    type="number"
+                    name="metascore"
+                    placeholder="Metascore"
+                    value={movie.metascore}
+                    onChange={handleChange}
+                />
+                <hr />
+                {movie.stars.map((actor, key) => {
+                    return (
+                        <input
+                            key={key}
+                            type="text"
+                            name="actor"
+                            id={key}
+                            placeholder={actor}
+                            value={actor}
+                            onChange={handleChange}
+                        />
+                    );
+                })}
+                <hr />
                 <input type="submit" />
             </form>
         </div>
